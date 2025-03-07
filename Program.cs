6440f61939f0ee82c4a38e1cebf4ff3fc54c25ad
Ilya Boiko <boiko.ilya.evg@gmail.com>
2025-03-07 12:09:02 +0500

    13.6.1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace SFyCSm013p6e1;
+
+class Program
+{
+    /// <summary>
+    /// Процедура реализующая основной алгоритм работы приложения.
+    /// Замер времени выполнения операций вставки
+    /// </summary>
+    static void PerformTiming()
+    {
+        // Путь к файлу в который будет выполнятся вставка
+        string filePath = Path.Combine(Directory.GetCurrentDirectory(), "input.txt"); 
+
+        var list = new List<String>();
+        var linkedList = new LinkedList<String>();
+
+        using (StreamReader stream = File.OpenText(filePath))
+        {
+            string? line = "";
+
+            while ((line = stream.ReadLine()) != null)
+            {
+                list.Add(line);
+                linkedList.AddLast(line);
+            }
+        }
+
+        var watch = new Stopwatch();
+
+        watch.Start();
+        list.Insert(0, "");
+        watch.Stop();
+
+        Console.WriteLine("Замер операции \"list.Insert\": {0} мс.", watch.Elapsed.TotalMilliseconds);
+
+        watch.Start();
+        linkedList.AddFirst("");
+        watch.Stop();
+
+        Console.WriteLine("Замер операции \"linkedList.AddFirst\": {0} мс.", watch.Elapsed.TotalMilliseconds);
+    }
+
+    /// <summary>
+    /// Главная точка входа приложения
+    /// </summary>
+    /// <param name="args">Аргументы командной строки при запуске приложения.</param>
+    static void Main(string[] args)
+    {
+        PerformTiming();
+    }
+}
